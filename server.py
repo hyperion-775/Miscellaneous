284e8521e56f4fc8e20eae4685b7e0916902c426
hyperion-775 <93292719+hyperion-775@users.noreply.github.com>
2022-09-25 15:02:15 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+x1=str(.51*24)
+x2=str(.51*24*31)
+print('How much does it cost to operate one server per day? ' + x1)
+print('How much does it cost to operate one server per month? ' + x2)
+
+x3=str(.51*24*20)
+x4=str(.51*24*20*31)
+x5=str(918/(.51*24))
+
+print('How much does it cost to operate twenty server per day? ' + x3)
+print('How much does it cost to operate twenty server per month? ' + x4)
+print('How many days can I operate one server with $918? ' + x5)
